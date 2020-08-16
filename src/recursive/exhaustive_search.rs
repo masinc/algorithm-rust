2c67072d302a82c8f72b89c40da8b5fb990ea16c
masinc <masinc000@gmail.com>
2020-08-16 13:47:05 +0900

    fix compute

--- src/recursive/exhaustive_search.rs
+++ src/recursive/exhaustive_search.rs
@@ -5,11 +5,11 @@ use std::{
 };
 
 pub fn compute(seq: &[usize], targets: &[usize]) -> Result<Vec<bool>, Box<dyn Error>> {
-    let combinations: Vec<Vec<Vec<&usize>>> = (1..(seq.len()))
-        .map(|i| seq.iter().combinations(i).into_iter().collect())
+    let combinations: Vec<Vec<Vec<&usize>>> = (0..seq.len())
+        .map(|i| seq.iter().combinations(i + 1).into_iter().collect())
         .collect();
     let compute_inner = |target| {
-        for i in 1..(seq.len()) {
+        for i in 0..(seq.len()) {
             for c in combinations.get(i).unwrap() {
                 if target == c.iter().map(|x| *x).sum::<usize>() {
                     return true;
